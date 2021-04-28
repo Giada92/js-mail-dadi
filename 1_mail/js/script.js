c4640b66fc97f9d15ef4a4babcf820b6f80e48a5
Giada92 <giada_meloni@virgilio.it>
2021-04-28 13:54:28 +0200

    Praparazione file

---
+++ 1_mail/js/script.js
@@ -0,0 +1,11 @@
+/* 1 - Mail
+Chiedi all'utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull'esito del controllo. */
+
+//chiedere all'utente la mail
+
+
+//creare array di mail
+
+
